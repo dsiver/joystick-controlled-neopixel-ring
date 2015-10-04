73d98258eaeab02c5c533038a30dd58663fd9227
David Siver <dsiver1@my.westga.edu>
2015-10-04 14:47:49 -0400

    Reading joystick x,y values and printing to serial monitor

--- joystick-controlled-neopixel-ring.ino
+++ joystick-controlled-neopixel-ring.ino
@@ -1,9 +1,30 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+ * Joystick Controlled Neopixel Ring
+ * Board: Arduino Esplora
+ *
+ * David Siver
+ * CS 4985
+ * Fall 2015
+ */
 
+#include <Esplora.h>
+
+#define DEBUG
+#define serialBaudRate 9600
+
+int xAxis, yAxis;
+
+void setup() {
+  xAxis = 0;
+  yAxis = 0;
+  Serial.begin(serialBaudRate);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  xAxis = Esplora.readJoystickX();
+  yAxis = Esplora.readJoystickY();
 
+  Serial.println("JoystickX: " + String(xAxis) + " " +
+               "JoystickY: " + String(yAxis));
+  delay(500);
 }
