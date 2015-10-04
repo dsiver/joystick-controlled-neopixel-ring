0a6c4caae88cd3c16232fc083d9b483f81ad4b36
David Siver <dsiver1@my.westga.edu>
2015-10-04 18:11:39 -0400

    Getting (x, y) coordinates from joystick. Converting point to radians.

--- joystick-controlled-neopixel-ring.ino
+++ joystick-controlled-neopixel-ring.ino
@@ -6,25 +6,45 @@
  * CS 4985
  * Fall 2015
  */
-
 #include <Esplora.h>
 
 #define DEBUG
-#define serialBaudRate 9600
+#define BAUD_RATE 9600
+#define TAN_MIN -90
+#define TAN_MAX 90
+#define NUM_PIXELS 16
+#define JOYSTICK_MAX 512
+#define JOYSTICK_MIN -512
 
-int xAxis, yAxis;
+int xCenter, yCenter, xReading, yReading;
 
 void setup() {
-  xAxis = 0;
-  yAxis = 0;
-  Serial.begin(serialBaudRate);
+  xCenter = Esplora.readJoystickX();
+  yCenter = Esplora.readJoystickY();
+  xReading = 0;
+  yReading = 0;
+  Serial.begin(BAUD_RATE);
 }
 
 void loop() {
-  xAxis = Esplora.readJoystickX();
-  yAxis = Esplora.readJoystickY();
-
-  Serial.println("JoystickX: " + String(xAxis) + " " +
-               "JoystickY: " + String(yAxis));
+  xReading = Esplora.readJoystickX();
+  yReading = Esplora.readJoystickY();
+  double joystickToRadians = calculateRadians(xReading, yReading);        
   delay(500);
 }
+
+double calculateRadians(int x, int y){
+  int xCalibrated = x - xCenter;
+  int yCalibrated = y - yCenter;
+  xCalibrated = -xCalibrated;
+  yCalibrated = -yCalibrated;
+  double calculation = atan2(yCalibrated, xCalibrated);
+
+  Serial.println("JoystickX: " + String(xReading) + "\t" +
+                 "JoystickY: " + String(yReading) + "\t" + 
+                 "atan2(" + String(yCalibrated) + ", " + String(xCalibrated) + ") = " +
+                 String(calculation));
+  
+  return calculation;
+}
+
