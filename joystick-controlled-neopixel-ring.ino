ddaeb208c6233199d691d0b1539b6e7d33221b75
David Siver <dsiver1@my.westga.edu>
2015-10-13 07:38:10 -0400

    added spinner method to test neopixel ring

--- joystick-controlled-neopixel-ring.ino
+++ joystick-controlled-neopixel-ring.ino
@@ -10,33 +10,43 @@
 #include <Adafruit_NeoPixel.h>
 
 #define DEBUG
-#define DELAY 500
+#define DELAY 50
 #define BAUD_RATE 9600
 #define TAN_MIN -90
 #define TAN_MAX 90
 #define NEOPIXEL_NUM_PIXELS 16
 #define NEOPIXEL_NO_PIXEL -1
 #define NEOPIXEL_PIN_NUMBER 1
+#define NEOPIXEL_BRIGHTNESS 20
 #define JOYSTICK_MAX 512
 #define JOYSTICK_MIN -512
 #define SENSITIVITY 400
 
-int xCenter, yCenter, xReading, yReading, pixelNumber;
-Adafruit_NeoPixel neopixelRing;
+int xCenter, yCenter, xReading, yReading, pixelNumber, oldPixelNumber;
+Adafruit_NeoPixel neoPixelRing;
+#ifdef DEBUG
+int up, down, left, right, oldUp, oldDown, oldLeft, oldRight;
+#endif
 
 void setup() {
   xCenter = Esplora.readJoystickX();
   yCenter = Esplora.readJoystickY();
   xReading = 0;
   yReading = 0;
-  pixelNumber = NEOPIXEL_NO_PIXEL;
   Serial.begin(BAUD_RATE);
-  neopixelRing = Adafruit_NeoPixel(NEOPIXEL_NUM_PIXELS, NEOPIXEL_PIN_NUMBER, NEO_GRB + NEO_KHZ800);
-  neopixelRing.begin();
-  neopixelRing.show();
+  pinMode(NEOPIXEL_PIN_NUMBER, OUTPUT);
+  neoPixelRing = Adafruit_NeoPixel(NEOPIXEL_NUM_PIXELS, NEOPIXEL_PIN_NUMBER), NEO_GRB + NEO_KHZ800;
+  neoPixelRing.begin();
+  neoPixelRing.show();
 }
 
 void loop() {
+  controlNeoPixelWithJoystick();
+  //spinner();
+  delay(DELAY);
+}
+
+void controlNeoPixelWithJoystick() {
   xReading = Esplora.readJoystickX();
   yReading = Esplora.readJoystickY();
   double joystickRadians;
@@ -46,17 +56,15 @@ void loop() {
     joystickDegrees = getDegrees(xReading, yReading);
     pixelNumber = map(joystickDegrees, 0, 330, 0, 15);
     pixelNumber = constrain(pixelNumber, 0, 15);
-    
+    neoPixelRing.setPixelColor(pixelNumber, neoPixelRing.Color(0, NEOPIXEL_BRIGHTNESS, 0));
+    neoPixelRing.show();
+
 #ifdef DEBUG
     Serial.println("joystickDegrees: " + String(joystickDegrees) + " " +
                    "pixelNumber: " + String(pixelNumber));
 #endif
 
   }
-  else {
-    pixelNumber = NEOPIXEL_NO_PIXEL;
-  }
-  delay(DELAY);
 }
 
 double getDegrees(int x, int y) {
@@ -74,5 +82,22 @@ double getDegrees(int x, int y) {
 #endif
 
   return calculation;
+}
+
+void spinner() {
+  for (int i = 0; i < neoPixelRing.numPixels(); i++) {
+    if (Esplora.readButton(SWITCH_UP) == LOW) {
+      neoPixelRing.setPixelColor(i, neoPixelRing.Color(NEOPIXEL_BRIGHTNESS, 0, 0));
+    }
+    else {
+      neoPixelRing.setPixelColor(i, neoPixelRing.Color(0, NEOPIXEL_BRIGHTNESS, 0));
+    }
+    neoPixelRing.show();
+    delay(20);
+    for (int j = neoPixelRing.numPixels() - 1; j >= 0; j--) {
+      neoPixelRing.setPixelColor(i, neoPixelRing.Color(0, 0, 0));
+      neoPixelRing.show();
+    }
+  }
 }
 
